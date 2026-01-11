000b5b0df8a8509a847d9f7892e7b95296775497
MICH233 <cnmichelle80@gmail.com>
2026-01-11 02:36:20 -0500

    Actualización

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,6 +1,17 @@
 const btn = document.getElementById("hamburguesa");
 const nav = document.getElementById("nav");
+const submenuToggle = document.querySelector(".submenu-toggle");
+const submenu = document.querySelector(".submenu");
 
+/* Abrir / cerrar menú principal */
 btn.addEventListener("click", () => {
     nav.classList.toggle("activo");
+});
+
+/* Submenú por CLICK (tablet y móvil) */
+submenuToggle.addEventListener("click", (e) => {
+    if (window.innerWidth <= 1024) {
+        e.preventDefault();
+        submenu.classList.toggle("activo");
+    }
 });
